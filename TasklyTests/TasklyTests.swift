b03f150d1e1569fb46d2da5fc1b3e49a98dd0519
Ritika Joshi <ritikajoshi141@gmail.com>
2025-10-16 16:07:28 -0700

    Initial Commit

---
+++ TasklyTests/TasklyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasklyTests.swift
+//  TasklyTests
+//
+//  Created by Ritika Joshi on 10/16/25.
+//
+
+import Testing
+@testable import Taskly
+
+struct TasklyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
